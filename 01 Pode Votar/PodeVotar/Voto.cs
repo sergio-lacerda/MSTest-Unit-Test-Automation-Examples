9de109c8e7e08f32e947de7dcc28c1738faeba13
Sérgio Lacerda <cursos@sergiolacerda.com.br>
2022-07-06 01:02:15 -0300

    Adding first test example - Pode Votar

---
+++ 01 Pode Votar/PodeVotar/Voto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodeVotar
+{
+    // This class defines the rules for brazilian people to vote.
+    public class Voto
+    {
+        /*
+           This method decides, based on a person age, if he or she is allowed to 
+           vote in an election:
+
+            + Age between 0 and 15 (including) --> Cannot vote ("Não pode votar!")
+            + Age between 16 (including) and 17 (including) --> Optional ("Voto opcional!")
+            + Age between 18 (including) and 70 --> Can vote ("Pode votar!")
+            + Age 70 or older --> Optional ("Voto opcional!")
+         */
+        public string podeVotar(int idade)
+        {
+            if (idade <= 15)
+                return "Não pode votar!";
+
+            if ((idade >= 16) && (idade <= 17))
+                return "Voto opcional!";
+
+            if ((idade >= 18) && (idade < 70))
+                return "Pode votar!";           
+
+            return "Voto opcional!";
+        }
+    }
+}
---
+++ 01 Pode Votar/PodeVotarTests/VotoUnitTests.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PodeVotar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodeVotar.Tests
+{
+    /*
+        This first example uses the simplest way to create the
+        tests, not implementing parameters (single conditions tests).
+        In this case, we are not testing yet for invalid age parameters.
+    */
+    [TestClass()]
+    public class VotoUnitTests
+    {
+        //Age = 11 --> Expected: Cannot vote ("Não pode votar!")
+        [TestMethod()]
+        public void Idade11_NaoDevePoderVotar()
+        {
+            //Arrange - Preparing test conditions
+            Voto voto = new Voto();
+            int idade = 11;
+            string esperado = "Não pode votar!";
+
+            //Act - Executing the test
+            string obtido = voto.podeVotar(idade);
+
+            //Assert - Comparing expected and obtained values
+            Assert.AreEqual(esperado, obtido);
+        }
+
+        //Age = 15 --> Expected: Cannot vote ("Não pode votar!")
+        [TestMethod()]
+        public void Idade15_NaoDevePoderVotar()
+        {
+            //Arrange - Preparing test conditions
+            Voto voto = new Voto();
+            int idade = 15;
+            string esperado = "Não pode votar!";
+
+            //Act - Executing the test
+            string obtido = voto.podeVotar(idade);
+
+            //Assert - Comparing expected and obtained values
+            Assert.AreEqual(esperado, obtido);
+        }
+
+        //Age = 16 --> Expected: Optional ("Voto opcional!")
+        [TestMethod()]
+        public void Idade16_DeveSerOpcional()
+        {
+            //Arrange - Preparing test conditions
+            Voto voto = new Voto();
+            int idade = 16;
+            string esperado = "Voto opcional!";
+
+            //Act - Executing the test
+            string obtido = voto.podeVotar(idade);
+
+            //Assert - Comparing expected and obtained values
+            Assert.AreEqual(esperado, obtido);
+        }
+
+        //Age = 17 --> Expected: Optional ("Voto opcional!")
+        [TestMethod()]
+        public void Idade17_DeveSerOpcional()
+        {
+            //Arrange - Preparing test conditions
+            Voto voto = new Voto();
+            int idade = 17;
+            string esperado = "Voto opcional!";
+
+            //Act - Executing the test
+            string obtido = voto.podeVotar(idade);
+
+            //Assert - Comparing expected and obtained values
+            Assert.AreEqual(esperado, obtido);
+        }
+
+        //Age = 18 --> Expected: Can vote ("Pode votar!")
+        [TestMethod()]
+        public void Idade18_DevePoderVotar()
+        {
+            //Arrange - Preparing test conditions
+            Voto voto = new Voto();
+            int idade = 18;
+            string esperado = "Pode votar!";
+
+            //Act - Executing the test
+            string obtido = voto.podeVotar(idade);
+
+            //Assert - Comparing expected and obtained values
+            Assert.AreEqual(esperado, obtido);
+        }
+
+        //Age = 21 --> Expected: Can vote ("Pode votar!")
+        [TestMethod()]
+        public void Idade21_DevePoderVotar()
+        {
+            //Arrange - Preparing test conditions
+            Voto voto = new Voto();
+            int idade = 21;
+            string esperado = "Pode votar!";
+
+            //Act - Executing the test
+            string obtido = voto.podeVotar(idade);
+
+            //Assert - Comparing expected and obtained values
+            Assert.AreEqual(esperado, obtido);
+        }
+
+        //Age = 69 --> Expected: Can vote ("Pode votar!")
+        [TestMethod()]
+        public void Idade69_DevePoderVotar()
+        {
+            //Arrange - Preparing test conditions
+            Voto voto = new Voto();
+            int idade = 69;
+            string esperado = "Pode votar!";
+
+            //Act - Executing the test
+            string obtido = voto.podeVotar(idade);
+
+            //Assert - Comparing expected and obtained values
+            Assert.AreEqual(esperado, obtido);
+        }
+
+        //Age = 70 --> Expected: Optional ("Voto opcional!")
+        [TestMethod()]
+        public void Idade70_DeveSerOpcional()
+        {
+            //Arrange - Preparing test conditions
+            Voto voto = new Voto();
+            int idade = 70;
+            string esperado = "Voto opcional!";
+
+            //Act - Executing the test
+            string obtido = voto.podeVotar(idade);
+
+            //Assert - Comparing expected and obtained values
+            Assert.AreEqual(esperado, obtido);
+        }
+
+        //Age = 85 --> Expected: Optional ("Voto opcional!")
+        [TestMethod()]
+        public void Idade85_DeveSerOpcional()
+        {
+            //Arrange - Preparing test conditions
+            Voto voto = new Voto();
+            int idade = 85;
+            string esperado = "Voto opcional!";
+
+            //Act - Executing the test
+            string obtido = voto.podeVotar(idade);
+
+            //Assert - Comparing expected and obtained values
+            Assert.AreEqual(esperado, obtido);
+        }
+
+    }
+}
